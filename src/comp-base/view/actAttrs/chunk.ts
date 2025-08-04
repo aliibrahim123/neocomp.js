6445b5be30a79d540ea77d0334e5c926509dbe7a
aliibrahim123 <ali.ibrahim.dev.1548@gmail.com>
2025-08-04 15:57:58 +0300

    fix bug in @chunk

--- src/comp-base/view/actAttrs/chunk.ts
+++ src/comp-base/view/actAttrs/chunk.ts
@@ -1,13 +1,13 @@
 import type { ChunkAction } from "../../action/chunk.ts";
 import { throw_chunk_attr_no_name } from "../errors.ts";
-import { getTarget, removeAttr, toFun } from "../walkInterface.ts";
+import { decodeAttrArg, getTarget, removeAttr, toFun } from "../walkInterface.ts";
 import { addActionAttr } from "./index.ts";
 
 export function addChunkAttr () {
 	addActionAttr('chunk', (node, attr, value, addAction, options) => {
 		const nameStart = attr.indexOf(':') + 1;
 		if (nameStart === 0) throw_chunk_attr_no_name();
-		const name = attr.slice(nameStart);
+		const name = decodeAttrArg(attr.slice(nameStart), options);
 		const exp = value ? (value.includes(';') ? value : 'return ' + value) : 'return {}';
 		addAction({ 
 			type: 'chunk', target: getTarget(node), name, 
